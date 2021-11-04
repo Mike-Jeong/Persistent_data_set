7e57b0885c378af0b980ec415aecd941a2b4e542
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-11-04 22:38:49 +0900

    Add codes for test BinarySearchTree in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       BinarySearchTree<String> tree = new BinarySearchTree<>();
+
+       tree.add("cow");
+       tree.add("fly");
+       tree.add("dog");
+       tree.add("bat");
+       tree.add("fox");
+       tree.add("cat");
+       tree.add("eel");
+       tree.add("ant");
+       tree.add("owl");
+
+       System.out.println("tree : " + tree.toString());
+       System.out.println("rootnode : " + tree.rootNode);
+       System.out.println();
+
     }
 }
