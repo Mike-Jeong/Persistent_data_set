e9edb5a7ec9a1a87bdf269eef35bf3ea3b3febfe
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-11-05 23:16:36 +0900

    Add persistent dyanamicset class

---
+++ src/PersistentDynamicSet.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class PersistentDynamicSet<E extends Comparable<E>> extends BinarySearchTree<E> {
+    public List<List<Node<E>>> hist = new LinkedList<>();
+    protected List<Node<E>> tempList = new LinkedList<>();
+    public LinkedList<Node<E>> treehistory = new LinkedList<>();
+
+    @Override
+    protected Node<E> add_pre(Node<E> node) {
+        if (node != null) {
+            treehistory.add(node);
+        }
+        return node;
+    }
+
+    @Override
+    protected Node<E> history_Nodeadd(Node<E> node) {
+        return node.clone();
+    }
+
+    @Override
+    protected Node<E> remove_pre(Node<E> node) {
+        if (node != null) {
+            treehistory.add(node);
+        }
+        return node;
+    }
+
+    @Override
+    protected Node<E> history_NodeRemove(Node<E> node) {
+        return node.clone();
+    }
+
+    @Override
+    protected Node<E> nodeDiscovered(Node<E> node) {
+        tempList.add(node);
+        return node;
+    }
+
+    @Override
+    public void nodeVersion(boolean result) {
+        if (result) {
+            hist.add(tempList);
+        }
+        tempList = new LinkedList<>();
+    }
+
+    public List<List<Node<E>>> gethist() {
+        return hist;
+    }
+
+}
